4d787f857062fc0b59a5828ce8dc4c6d2db757fe
Carlos-Morales <carlos7ma@correo.ugr.es>
2022-06-13 21:50:16 +0200

    Add break line

--- src/isaacbot.py
+++ src/isaacbot.py
@@ -12,7 +12,7 @@ load_dotenv(dotenv_path = '.env')
 TOKEN = os.getenv('TOKEN')
 
 if not TOKEN:
-	TOKEN = input('Please enter a valid Telegram Bot Token: ')
+	TOKEN = input('\nPlease enter a valid Telegram Bot Token: ')
 
 bot = telebot.TeleBot(TOKEN)
 controller = Controller()
